ed7728950559b1e490ef67d08363ce2eed5d3e73
tatiana <bosenko.tanya@gmail.com>
2026-05-10 17:28:16 +0300

    install and configure cypress

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  allowCypressEnv: false,
+  watchForFileChanges: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
